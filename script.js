3214eab50be936041f83ad72a22c26b56a10b240
shrikarch <shrikar.code@gmail.com>
2018-11-05 17:31:34 -0500

    added firebase auth

--- script.js
+++ script.js
@@ -1,106 +1,106 @@
 $(function () {
 
-    // Create a model for the services
-    var Service = Backbone.Model.extend({
+  // Create a model for the services
+  var Service = Backbone.Model.extend({
 
-        // Will contain three attributes.
-        // These are their default values
+    // Will contain three attributes.
+    // These are their default values
 
-        defaults: {
-            title: 'My service',
-            price: 100,
-            checked: false
-        },
+    defaults: {
+      title: 'My service',
+      price: 100,
+      checked: false
+    },
 
-        // Helper function for checking/unchecking a service
-        toggle: function () {
-            this.set('checked', !this.get('checked'));
-        }
-    });
+    // Helper function for checking/unchecking a service
+    toggle: function () {
+      this.set('checked', !this.get('checked'));
+    }
+  });
 
-    // Create a collection of services
-    var ServiceList = Backbone.Collection.extend({
+  // Create a collection of services
+  var ServiceList = Backbone.Collection.extend({
 
-        // Will hold objects of the Service model
-        model: Service,
-        /* url: "https://testbackbone-67782.firebaseio.com",*/
-        // Return an array only with the checked services
-         getChecked: function(){
-             return this.where({checked:true});
-         }
-    });
+    // Will hold objects of the Service model
+    model: Service,
+    /* url: "https://testbackbone-67782.firebaseio.com",*/
+    // Return an array only with the checked services
+    getChecked: function(){
+      return this.where({checked:true});
+    }
+  });
 
-    // Prefill the collection with a number of services.
-    var services = new ServiceList([
-        new Service({ title: 'web development', price: 200 }),
-        new Service({ title: 'web design', price: 250 }),
-        new Service({ title: 'photography', price: 100 }),
-        new Service({ title: 'coffee drinking', price: 10 })
-        // Add more here
-    ]);
+  // Prefill the collection with a number of services.
+  var services = new ServiceList([
+    new Service({ title: 'web development', price: 200 }),
+    new Service({ title: 'web design', price: 250 }),
+    new Service({ title: 'photography', price: 100 }),
+    new Service({ title: 'coffee drinking', price: 10 })
+    // Add more here
+  ]);
 
-    // This view turns a Service model into HTML. Will create LI elements.
-    var ServiceView = Backbone.View.extend({
-        tagName: 'li',
-        events: {
-            'click': 'toggleService'
-        },
-        initialize: function () {
-            // Set up event listeners. The change backbone event
-            // is raised when a property changes (like the checked field)
-            this.listenTo(this.model, 'change', this.render);
-        },
-        render: function () {
-            // Create the HTML
-            this.$el.html('<input type="checkbox" value="1" name="' + this.model.get('title') + '" /> ' + this.model.get('title') + '<span>$' + this.model.get('price') + '</span>');
-            this.$('input').prop('checked', this.model.get('checked'));
-            // Returning the object is a good practice
-            // that makes chaining possible
-            return this;
-        },
-        toggleService: function () {
-            this.model.toggle();
-        }
-    });
+  // This view turns a Service model into HTML. Will create LI elements.
+  var ServiceView = Backbone.View.extend({
+    tagName: 'li',
+    events: {
+      'click': 'toggleService'
+    },
+    initialize: function () {
+      // Set up event listeners. The change backbone event
+      // is raised when a property changes (like the checked field)
+      this.listenTo(this.model, 'change', this.render);
+    },
+    render: function () {
+      // Create the HTML
+      this.$el.html('<input type="checkbox" value="1" name="' + this.model.get('title') + '" /> ' + this.model.get('title') + '<span>$' + this.model.get('price') + '</span>');
+      this.$('input').prop('checked', this.model.get('checked'));
+      // Returning the object is a good practice
+      // that makes chaining possible
+      return this;
+    },
+    toggleService: function () {
+      this.model.toggle();
+    }
+  });
 
-    // The main view of the application
-    var App = Backbone.View.extend({
-        // Base the view on an existing element
-        el: $('#main'),
-        initialize: function () {
-            // Cache these selectors
-            this.total = $('#total span');
-            this.list = $('#services');
-            // Listen for the change event on the collection.
-            // This is equivalent to listening on every one of the
-            // service objects in the collection.
-            this.listenTo(services, 'change', this.render);
-            // Create views for every one of the services in the
-            // collection and add them to the page
-            services.each(function (service) {
-                var view = new ServiceView({ model: service });
-                this.list.append(view.render().el);
-            }, this);   // "this" is the context in the callback
-        },
-        render: function () {
-            // Calculate the total order amount by agregating
-            // the prices of only the checked elements
-            var total = 0;
-            _.each(services.getChecked(), function (elem) {
-                total += elem.get('price');
-            });
-            // Update the total price
-            this.total.text('$' + total);
-            return this;
-        }
-    });
-    new App();
+  // The main view of the application
+  var App = Backbone.View.extend({
+    // Base the view on an existing element
+    el: $('#main'),
+    initialize: function () {
+      // Cache these selectors
+      this.total = $('#total span');
+      this.list = $('#services');
+      // Listen for the change event on the collection.
+      // This is equivalent to listening on every one of the
+      // service objects in the collection.
+      this.listenTo(services, 'change', this.render);
+      // Create views for every one of the services in the
+      // collection and add them to the page
+      services.each(function (service) {
+        var view = new ServiceView({ model: service });
+        this.list.append(view.render().el);
+      }, this);   // "this" is the context in the callback
+    },
+    render: function () {
+      // Calculate the total order amount by agregating
+      // the prices of only the checked elements
+      var total = 0;
+      _.each(services.getChecked(), function (elem) {
+        total += elem.get('price');
+      });
+      // Update the total price
+      this.total.text('$' + total);
+      return this;
+    }
+  });
+  new App();
 });
 
 var MyTodo = Backbone.Model.extend({
-    defaults: {
-        title: "New Todo"
-    }
+  defaults: {
+    title: "New Todo"
+  }
 });
 
 // var MyTodoCollection = Backbone.Firebase.Collection.extend({
@@ -110,15 +110,14 @@ var MyTodo = Backbone.Model.extend({
 
 
 var config = {
-    apiKey: "AIzaSyBAbF5ELmnEvFaKdRMVDJth729qUpjVWI4",
-    authDomain: "testbackbone-67782.firebaseapp.com",
-    databaseURL: "https://testbackbone-67782.firebaseio.com",
-    projectId: "testbackbone-67782",
-    storageBucket: "testbackbone-67782.appspot.com",
-    messagingSenderId: "106173591594"
-  };
+  apiKey: "AIzaSyBAbF5ELmnEvFaKdRMVDJth729qUpjVWI4",
+  authDomain: "testbackbone-67782.firebaseapp.com",
+  databaseURL: "https://testbackbone-67782.firebaseio.com",
+  projectId: "testbackbone-67782",
+  storageBucket: "testbackbone-67782.appspot.com",
+  messagingSenderId: "106173591594"
+};
 window.firebase.initializeApp(config);
-
 
 var MyTodoCollection = Backbone.Firebase.Collection.extend({
   url:"/todo", // This would tell the collection to use http://YOUR-URL.firebaseio.com/todos
@@ -139,31 +138,63 @@ var TodoView = Backbone.View.extend({
 
 // The main view for the application
 var MyAppView = Backbone.View.extend({
-    el: $('#mytodoapp'),
-    events: {
-        "click #add-todo": "createNewTodo",
-    },
-    initialize: function () {
-        this.list = this.$("#todo-items");
-        // Input for new Todos
-        this.input = this.$("#add-todo");
-        this.listenTo(this.collection, 'add', this.addOne);
-    },
-    addOne: function (todo) {
-        var view = new TodoView({ model: todo });
-        this.list.append(view.render().el);
-    },
-    createNewTodo: function (e) {
-        // Ensure input is not empty
-        if (!this.input.val()) { return; }
-        this.collection.create({ title: this.input.val() });
-        this.input.val('');
-    }
+  el: $('#mytodoapp'),
+  events: {
+    "click #add-todo": "createNewTodo",
+  },
+  initialize: function () {
+    this.list = this.$("#todo-items");
+    // Input for new Todos
+    this.input = this.$("#add-todo");
+    this.listenTo(this.collection, 'add', this.addOne);
+  },
+  addOne: function (todo) {
+    var view = new TodoView({ model: todo });
+    this.list.append(view.render().el);
+  },
+  createNewTodo: function (e) {
+    // Ensure input is not empty
+    if (!this.input.val()) { return; }
+    this.collection.create({ title: this.input.val() });
+    this.input.val('');
+  }
 });
 
 function initFirebase() {
-    var collection = new MyTodoCollection();
-    var app = new MyAppView({ collection: collection });
+  var collection = new MyTodoCollection();
+  var app = new MyAppView({ collection: collection });
+  firebaseauth();
 }
 
 $(function () { initFirebase() });
+
+function firebaseauth(){
+  console.log("Firebase auth");
+  var email = 'schonkar@miraclesoft.com';
+  var password = 'Miracle@123';
+  firebase.auth().signInWithEmailAndPassword(email, password)
+  .then(function(data){
+    console.log("loggedin");
+    console.log(data);
+  })
+  .catch(function(err){
+    console.log("User not found");
+    console.log(err);
+    if(!err){
+    }
+  });
+}
+
+$(document).ready(function(){
+  // console.log("jquery init");
+/*  var email = 'schonkar@miraclesoft.com';
+  var password = 'Miracle@123';
+  firebase.auth().signInWithEmailAndPassword(email, password).catch(function(err){
+    console.log(err);
+    if(!err){
+      console.log("logged in");
+    }
+  });
+  */
+
+})
